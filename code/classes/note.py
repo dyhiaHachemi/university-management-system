443f882f84d451de9f3dd16de411f0388c1b9459
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-04 05:12:28 +0100

    Ajout de la classe Note

---
+++ code/classes/note.py
@@ -0,0 +1,8 @@
+"""
+    class Note(value, module)
+    module est un objet de type Module uttilisé pour le calcul de la moyenne 
+"""
+class Note:
+    def __init__(self, value, module):
+        self.value = value        
+        self.module = module    
