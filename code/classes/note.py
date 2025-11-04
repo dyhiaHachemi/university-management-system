e86915501968be7f84cb1edb6e7be1f8a081ef2e
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-04 05:26:24 +0100

    Importation de la classe Module et représentation en chaîne de caractères des objet

--- code/classes/note.py
+++ code/classes/note.py
@@ -1,3 +1,4 @@
+from classes.module import Module
 """
     class Note(value, module)
     module est un objet de type Module uttilisé pour le calcul de la moyenne 
@@ -6,3 +7,7 @@ class Note:
     def __init__(self, value, module):
         self.value = value        
         self.module = module    
+    
+    #représentation en chaîne de caractères d'un objet
+    def __str__(self):
+        return(f"{self.module.name} : {self.value} /Coefficient: {self.module.coef}")
