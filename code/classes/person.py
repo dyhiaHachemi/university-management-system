c5aabba53be32c2418febf88401907f17950a996
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-04 15:54:18 +0100

    Ajout d'une classe mère Person

---
+++ code/classes/person.py
@@ -0,0 +1,11 @@
+"""
+    class mère Person (name, email, phoneNumber)
+"""
+
+class Person:
+    def __init__(self, name, phoneNumber, email):
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.email = email
+
+    
