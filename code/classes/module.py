4cc9990ec712f1d55e1e36c0f3f30bd718408256
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-04 05:11:47 +0100

    Ajout de la classe Module

---
+++ code/classes/module.py
@@ -0,0 +1,8 @@
+"""
+    class Module (Name, Coef)
+"""
+class Module:
+    # constructeur de la classe Module
+    def __init__(self, Name, Coef):
+        self.Name = Name
+        self.Coef = Coef
