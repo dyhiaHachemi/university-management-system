fa80ed179cb1508ec730f48419c459a53bb6e599
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-16 20:02:44 +0100

    Teste des classes year depChief

--- code/main.py
+++ code/main.py
@@ -1,11 +1,9 @@
-"""
-pour tester la classe Student il faut avoir d'abord creer des notes et des modules
-"""
-
 # importation des differentes classes
 from classes.module import Module
 from classes.note import Note
 from classes.student import Student
+from classes.year import Year
+from classes.depChief import DepChief
 
 if __name__ == "__main__":
     
@@ -16,16 +14,103 @@ if __name__ == "__main__":
     note1 = Note(14, module1)
     note2 = Note(12, module2)
     # affichage du module et de la note 
+    print("Liste des modules:")
     print(module1)
+    print(module2)
+    print("--------------------")
+    print("Liste des notes:")
     print(note1)
+    print(note2)
+    print("--------------------")
 
     # teste de la classe Student
     # Création d’un étudiant
-    etudiant = Student("Tizi-Ouzou", "Dyhia", "dyhia@gmail.com", "0555542355")
-    # ajout des notes a la liste des notes de l'etudiant
-    etudiant.addNote(note1)
-    etudiant.addNote(note2)
-    # affichage des informations de l'etudiant crée 
-    print(etudiant)
+    etudiant1 = Student("Tizi-Ouzou","Dyhia", "dyhia@gmail.com", "0555542355")
+    etudiant2 = Student("Tizi-Ouzou","Ali", "ali@mail.com", "0555542786")
+    etudiant3 = Student("Tizi-Ouzou","Sara", "sara@mail.com", "0675542355")
+    etudiant4 = Student("Tizi-Ouzou","Sabrinel", "sabrinel@mail.com", "0725542359")
+
+    # ajout des notes a la liste des notes 
+    etudiant1.addNote(note1)
+    etudiant1.addNote(note2)
+    etudiant2.addNote(Note(14,module1))
+    etudiant2.addNote(Note(16,module2))
+    etudiant3.addNote(Note(10,module1))
+    etudiant3.addNote(Note(12,module2))
+    etudiant4.addNote(Note(6,module1))
+    etudiant4.addNote(Note(1,module2))
+
+    # affichage des informations de etudiants crées
+    print("Liste des etudiants:")
+    print(etudiant1)
+    print(etudiant2)
+    print(etudiant3)
+    print(etudiant4)
+    print("--------------------")
+
     # affichage de la liste des notes 
-    etudiant.displayNotes()
+    print("Liste des notes:")
+    etudiant1.displayNotes()
+    etudiant2.displayNotes()
+    etudiant3.displayNotes()
+    etudiant4.displayNotes()
+    print("--------------------")
+
+    # teste de la classe Year
+    # Creation des sections
+    l1 = Year("L1 Informatique")
+    l2 = Year("L2 Informatique")
+    l3 = Year("L3 Informatique")
+
+    # ajout des etudiants et modules aux differentes sections
+    l1.addModule(module1)
+    l1.addModule(module2)
+    l1.addStudent(etudiant1)
+    l1.addStudent(etudiant2)
+    l2.addStudent(etudiant3)
+    l2.addStudent(etudiant4)
+
+    # afficher liste des etudiants de chaque section
+    print("Liste des etudiants de chaque section:")
+    l1.displayStudents()
+    print("--------------------")
+    l2.displayStudents()
+    print("--------------------")
+    l3.displayStudents()
+    print("--------------------")
+
+    # supprimer un etudiant 
+    print("Suppression de l'etudiant: ",etudiant1.name ) 
+    l1.removeStudent(etudiant1)
+    print("--------------------")
+    # afficher la liste des etudiants de la section apres suppression
+    l1.displayStudents()
+    print("--------------------")
+
+    # teste de la classe DepChief
+    # creation d'un Chef de département
+    chef = DepChief("Dr. Karim", "karim@univ.dz","0765432345")
+    # affichage de information du chef de departement
+    print("Chef de departement: ")
+    print(chef)
+    print("--------------------")
+
+    # remplissage de la liste des moyennes 
+    chef.calculAvg(l1)
+    chef.calculAvg(l2)
+    chef.calculAvg(l3)
+    # affichage des listes moyennes calculées
+    print("liste des moyennes de chaque section:")
+    l1.displayMarks()
+    print("--------------------")
+    l2.displayMarks()
+    print("--------------------")
+    l3.displayMarks()
+    print("--------------------")
+ 
+    # passer un etudiant au next year
+    l2.sendStudentToNextYear(etudiant4, l3)
+    print("--------------------")
+    # affichage de la liste des etudiants apres pasasge d'un etudiant au next year
+    l2.displayStudents()
+    l3.displayStudents()
