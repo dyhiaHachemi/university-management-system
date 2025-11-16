89225a5017ea097c0743b2a23bc715d6575282e9
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-16 19:22:52 +0100

    Représentation en chaîne de caractères des objets

--- code/classes/depChief.py
+++ code/classes/depChief.py
@@ -6,6 +6,11 @@ class DepChief(Person):
     def __init__(self, name, email, phoneNumber):
         # initialiser les attributs de la classe parente Person
         super().__init__(name, email, phoneNumber)
+
+    # représentation en chaîne de caractères d'un objet
+    def __str__(self):
+        return (f" - Nom du Chef de Departement: {self.name} \n Email: {self.email} \n Numero de Telephone: {self.phoneNumber}")
+    
     # fonction pour le calcul etle remplissage de la liste des moyennes
     def calculAvg(self,year):
         year.listOfMarks = [] # vides la liste des moyennes avant de recaluler pour chaque year
