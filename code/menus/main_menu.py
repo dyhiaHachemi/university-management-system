878197f53e104499167c48f8975bb758db1e403a
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-20 13:29:53 +0100

    le main_menu est fonctionnel

--- code/menus/main_menu.py
+++ code/menus/main_menu.py
@@ -1,21 +1,38 @@
 from classes.university import University
+from menus.faculty_menu import faculty_menu
+
 def main_menu():
 
-    print("---------- Systeme de gestion d'université ----------")
+    print("---------- Systeme de gestion d'universite ----------")
     
     # creation d'une université au démarage
-    university = University("Universite Mouloud Mammeri", "026123456", "contact@ummto.dz", "Tizi-Ouzou")
+    universite = University("Universite Mouloud Mammeri", "026123456", "contact@ummto.dz", "Tizi-Ouzou")
        
     # affichage du menu principal
     while True :
-        print("\ === MENU PRINCIPAL ===")
-        print("1. Gestion des facultés")
-        print("2. Gestion des départements")
+        print("\n === MENU PRINCIPAL ===")
+        print("1. Gestion des facultes")
+        print("2. Gestion des departements")
         print("3. Gestion des sections")
-        print("4. Gestion des étudiants")
-        print("5. Affichage de l'université")
+        print("4. Gestion des etudiants")
+        print("5. Affichage de l'universite")
         print("0. Quitter")
 
-        
+        choix = input("Veuillez saisir votre choix (0 - 5) : ")
 
-        
+        match choix :
+            case "1" :
+                faculty_menu(universite)
+            case "2" :
+                departement_menu(universite)
+            case "3" :
+                year_menu(universite)
+            case "4" :
+                student_menu(universite)
+            case "5" :
+                print(universite)
+            case "0" :
+                print("Au-revoir!")
+                break
+            case _ :
+                print("Choix invalid! veuillez reessayer")
