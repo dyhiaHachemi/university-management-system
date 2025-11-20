2cf91ba8b6db69f94bdc7f6731c1d19d79abf51e
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-20 13:59:47 +0100

    Ajout d'un menu d'acceuil pour creer une nouvelle universite

--- code/main.py
+++ code/main.py
@@ -1,7 +1,34 @@
 from menus.main_menu import main_menu
+from classes.university import University
+
+def create_university():
+    """
+    Cree une universite au demarrage du systeme
+    """
+    print("\n Creation d'une nouvelle universite:")
+    name = input("Entrez le nom de l'universite: ")
+    address = input("Entrez l'addresse de l'universite: ")
+    email = input("Entrez l'email de l'universite: ")
+    phone_number = input("Entrez le numero de telephone de l'universite: ")
+    return University(name, phone_number , email, address)
 
 def main():
-    main_menu() # appel de la fonction main_menu pour afficher le menu principal de l'application
+    print("---------- Systeme de gestion d'universite ----------")
+    while True :
+        print("\n === MENU D'ACCEUIL ===")
+        print("1. Creer une nouvelle université")
+        print("0. Quitter")
+        choix = input("Entrez votre choix (0/1):")
+        match choix:
+            case "1":
+                universite = create_university()
+                print(f"Universite {universite.name} cree avec succes!")
+                main_menu(universite) # appel de la fonction main_menu pour afficher le menu principal de l'application
+            case "0":
+                print("Au revoir!")
+                break
+            case _ :
+                print("Choix invalid! Veuillez reessayer") 
 
 if __name__ == "__main__":
     main()
