6c889dfa9d6c297220eb5c53b12e6e083b763fcd
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-26 21:54:21 +0100

    Affectation d'un directeur a l'universite

--- code/main.py
+++ code/main.py
@@ -1,9 +1,10 @@
 from menus.main_menu import main_menu
 from classes.university import University
+from classes.univDirector import UnivDirector
 
-def create_university():
+def create_university(universite):
     """
-    Cree une universite au demarrage du systeme
+    Creer une universite au demarrage du systeme
     """
     print("Creation d'une nouvelle universite:")
     name = input("Entrez le nom de l'universite: ")
@@ -12,9 +13,25 @@ def create_university():
     phone_number = input("Entrez le numero de telephone de l'universite: ")
     return University(name, phone_number , email, address)
 
+def set_university_director(universite):
+    """
+    Creer un directeur d'universite au demarrage du systeme 
+    apres la creation d'une universite
+    """
+    print(f"Affectation d'un directeur a {universite.name}")
+    name = input("Entrez le nom du directeur: ")
+    email = input("Entrez l'email du directeur: ")
+    phone_number = input("Entrez le numero de telephone du directeur: ")
+    university_director = UnivDirector(name, phone_number, email, universite)
+    universite.set_univ_director(university_director)
+    return
+
 def main():
     universite = create_university()
     print(f"{universite.name} cree avec succes!")
+    # l'universite cree affecte un directeur a l'universite 
+    set_university_director(universite)
+    #print(f"Le directeur {university_director.name} a ete affecte a {universite.name} avec succes!")
     main_menu(universite) # appel de la fonction main_menu pour afficher le menu principal de l'application
 
 if __name__ == "__main__":
