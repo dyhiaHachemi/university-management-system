1193b28c7555715b66ae56f42d26573b6cc552d5
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-11-26 21:37:51 +0100

    Avoir une nouvelle universite a chaque execution du programme

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ def create_university():
     """
     Cree une universite au demarrage du systeme
     """
-    print("\n Creation d'une nouvelle universite:")
+    print("Creation d'une nouvelle universite:")
     name = input("Entrez le nom de l'universite: ")
     address = input("Entrez l'addresse de l'universite: ")
     email = input("Entrez l'email de l'universite: ")
@@ -13,22 +13,9 @@ def create_university():
     return University(name, phone_number , email, address)
 
 def main():
-    print("---------- Systeme de gestion d'universite ----------")
-    while True :
-        print("\n === MENU D'ACCEUIL ===")
-        print("1. Creer une nouvelle université")
-        print("0. Quitter")
-        choix = input("Entrez votre choix (0/1):")
-        match choix:
-            case "1":
-                universite = create_university()
-                print(f"Universite {universite.name} cree avec succes!")
-                main_menu(universite) # appel de la fonction main_menu pour afficher le menu principal de l'application
-            case "0":
-                print("Au revoir!")
-                break
-            case _ :
-                print("Choix invalid! Veuillez reessayer") 
+    universite = create_university()
+    print(f"{universite.name} cree avec succes!")
+    main_menu(universite) # appel de la fonction main_menu pour afficher le menu principal de l'application
 
 if __name__ == "__main__":
     main()
